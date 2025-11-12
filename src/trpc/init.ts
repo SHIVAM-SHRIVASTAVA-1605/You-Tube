0f7a5e5221f612c11b0d3989df1cc2ce150dedb8
SHIVAM-SHRIVATSAVA-1605 <shivam091204@gmail.com>
2025-11-12 12:12:18 +0530

    06 TRPC setup

---
+++ src/trpc/init.ts
@@ -0,0 +1,22 @@
+import { initTRPC } from '@trpc/server';
+import { cache } from 'react';
+export const createTRPCContext = cache(async () => {
+    /**
+     * @see: https://trpc.io/docs/server/context
+     */
+    return { userId: 'user_123' };
+});
+// Avoid exporing the entire t-object
+// since it's not very descriptive
+// for instancee, the use o a t variable
+// is common in il8n libraries.
+const t = initTRPC.create({
+    /**
+     * @see https://trpc.io/docs/server/data-transformers
+     */
+    // transformer: superjson,
+});
+// Base router and procedure helpers
+export const createTRPCRouter = t.router;
+export const createCallerFactory = t.createCallerFactory;
+export const baseProcedure = t.procedure;
