5d5b80be3ab95f6ea8d1647328ffde872d79be17
SHIVAM-SHRIVATSAVA-1605 <shivam091204@gmail.com>
2026-01-01 23:59:08 +0530

    Video page 75% done

--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
 import { VideoGetOneOutput } from "../../types";
 import { UserAvatar } from "@/components/user-avatar";
+import { useAuth } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button";
+import { UserInfo } from "@/modules/users/ui/components/user-info";
 
 interface VideoOwnerProps {
     user: VideoGetOneOutput["user"];
@@ -8,17 +12,38 @@ interface VideoOwnerProps {
 };
 
 export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
+    const { userId } = useAuth();
+
     return (
         <div className="flex items-center sm:items-start justify-between sm:justify-start gap-3 min-w-0">
             <Link href={`/users/${user.id}`}>
                 <div className="flex items-center gap-3 min-w-0">
                     <UserAvatar size="lg" imageUrl={user.imageUrl} name={user.name} />
+                    <UserInfo size="lg" name={user.name}/>
                     <span className="text-sm text-muted-foreground line-clamp-1">
                         {/* TODO: properly fill subscriber count */}
                         {0} subscribers
                     </span>
                 </div>
             </Link>
+            {userId === user.clerkId ? (
+                <Button
+                    variant="secondary"
+                    className="rounded-full"
+                    asChild
+                >
+                    <Link href={`/studio/videos/${videoId}`}>
+                        Edit Video
+                    </Link>
+                </Button>
+            ) : (
+                <SubscriptionButton 
+                    onClick={() => {}}
+                    disabled={false}
+                    isSubscribed={false}
+                    className="flex-none"
+                />
+            )}
         </div>
     );
 };
